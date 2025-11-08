374acedaba8403008d99621f791a6e2b46a200c2
Izanagi WebSmith <kaganayse015@gmail.com>
2025-11-08 23:28:47 +0300

    Update ProtocolForwarder.sol

--- contracts/ProtocolForwarder.sol
+++ contracts/ProtocolForwarder.sol
@@ -3,6 +3,12 @@ pragma solidity 0.8.19;
 
 import {Forwarder} from "@opengsn/contracts/src/forwarder/Forwarder.sol";
 
+/**
+ * @title ProtocolForwarder
+ * @dev A minimal contract extending OpenGSN's Forwarder.
+ *      Used to relay meta-transactions within the protocol.
+ *      Enables gasless transaction forwarding through the OpenGSN infrastructure.
+ */
 contract ProtocolForwarder is Forwarder {
     constructor() Forwarder() {}
 }
